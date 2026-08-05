78d73bf879d0b6f1aff397877846d551682cbb0b
Mateus Zancheta <mateus.zancheta@uniplaclages.edu.br>
2026-08-04 23:35:07 -0300

    Estudo com Switch

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 
 
+//Estudo com Switch
 
-//Prática Loops
+function pedirItem() {
+    var valor = prompt('Digite um valor de 1 a 4: ');
 
-//Para comentar tudo CTRL + K + C. Para Descomentar tudo CTRL + K + U.
+    switch(Number(valor)) {
+      case 1:
+        alert('Você escolheu 1 = Suco');
+        break;
+      case 2:
+        alert('Você escolheu 2 = Água gelada');
+        break;
+      case 3:
+        alert('Você escolheu 3 = Sorvete');
+        break;
+      case 4:
+        alert('Você escolheu 4 = Chamar o garçom');
+        break;
 
-// var x= 0;
-
-// while(x < 10){
-//     document.write("<br> O valor de X é: " + x);
-
-//     //Auto-incremento no var x
-//     x++;
-// }
-
-var valor = 25;
-
-for(a = 0; a < valor; a++){
-    document.write('<br> O valor de A é: ' + a);
+      default:
+        alert('Opção inválida! Escolha um número entre 1 a 4.');
+        break;
+    }
 }
