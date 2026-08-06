3376501db44c4984a2c9f5b5d6108b5b7b446636
Mateus Zancheta <mateus.zancheta@uniplaclages.edu.br>
2026-08-05 23:57:33 -0300

    Prática: Calculadora IMC

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
+/*
+Abaixo de 17, muito abaixo do peso
+Entre 17 e 18,49, abaixo do peso
+Entre 18,5 e 24,99, peso normal
+Entre 25 e 29,99, acima do peso
 
+Calculo -> Peso / (Altura * Altura)
+*/
 
-//Estudo com Switch
+var peso;
+var altura;
+var imc;
+var resultado;
 
-function pedirItem() {
-    var valor = prompt('Digite um valor de 1 a 4: ');
+function calcularIMC(event) {
+  event.preventDefault();
 
-    switch(Number(valor)) {
-      case 1:
-        alert('Você escolheu 1 = Suco');
-        break;
-      case 2:
-        alert('Você escolheu 2 = Água gelada');
-        break;
-      case 3:
-        alert('Você escolheu 3 = Sorvete');
-        break;
-      case 4:
-        alert('Você escolheu 4 = Chamar o garçom');
-        break;
+  peso = document.getElementById('peso').value;
+  altura = document.getElementById('altura').value;
+  resultado = document.getElementById('resultado');
+ 
+  imc = peso / (altura * altura);
 
-      default:
-        alert('Opção inválida! Escolha um número entre 1 a 4.');
-        break;
-    }
+  if(imc < 17) {
+    resultado.innerHTML = '</br> Seu IMC é: ' + imc.toFixed(2) + '</br> Muito abaixo do peso!';
+  }
+  else if(imc > 17 && imc <= 18.49) {
+    resultado.innerHTML = '</br>Seu IMC é: ' + imc.toFixed(2) + '</br>Abaixo do peso!';
+  }
+  else if(imc >= 18.5 && imc <= 24.99) {
+    resultado.innerHTML = '</br> Seu IMC é: ' + imc.toFixed(2) + '</br>Peso normal!';
+  }
+  else if(imc >= 25 && imc <= 29.99) {
+    resultado.innerHTML = '</br> Seu IMC é: ' + imc.toFixed(2) + '</br> Acima do peso!';
+  }
+
+  document.getElementById('peso').value = '';
+  document.getElementById('altura').value = '';
 }
