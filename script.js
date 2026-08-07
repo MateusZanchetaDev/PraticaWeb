0dd885d9c5ecf29844ea59aaa7e8f4241d297c81
Mateus Zancheta <mateus.zancheta@uniplaclages.edu.br>
2026-08-06 23:34:06 -0300

    Estudo sobre setInterval e Timeout

--- script.js
+++ script.js
@@ -1,39 +1,14 @@
-/*
-Abaixo de 17, muito abaixo do peso
-Entre 17 e 18,49, abaixo do peso
-Entre 18,5 e 24,99, peso normal
-Entre 25 e 29,99, acima do peso
+// setInterval e Timeout
 
-Calculo -> Peso / (Altura * Altura)
-*/
-
-var peso;
-var altura;
-var imc;
-var resultado;
-
-function calcularIMC(event) {
-  event.preventDefault();
-
-  peso = document.getElementById('peso').value;
-  altura = document.getElementById('altura').value;
-  resultado = document.getElementById('resultado');
- 
-  imc = peso / (altura * altura);
-
-  if(imc < 17) {
-    resultado.innerHTML = '</br> Seu IMC é: ' + imc.toFixed(2) + '</br> Muito abaixo do peso!';
-  }
-  else if(imc > 17 && imc <= 18.49) {
-    resultado.innerHTML = '</br>Seu IMC é: ' + imc.toFixed(2) + '</br>Abaixo do peso!';
-  }
-  else if(imc >= 18.5 && imc <= 24.99) {
-    resultado.innerHTML = '</br> Seu IMC é: ' + imc.toFixed(2) + '</br>Peso normal!';
-  }
-  else if(imc >= 25 && imc <= 29.99) {
-    resultado.innerHTML = '</br> Seu IMC é: ' + imc.toFixed(2) + '</br> Acima do peso!';
-  }
-
-  document.getElementById('peso').value = '';
-  document.getElementById('altura').value = '';
+function acao() {
+  document.write('Executando... <br/>');
 }
+
+var timer = setInterval(() => {
+  document.write('Executando!! <br/>');
+}, 1000);
+
+
+setTimeout(() => {
+  document.write('Executou o Timeout! <br/>');
+}, 3000);
