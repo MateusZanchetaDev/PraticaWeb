b4242fb8442db7e522e1ba18b05481c8611681a9
Mateus Zancheta <mateus.zancheta@uniplaclages.edu.br>
2026-08-09 04:02:45 -0300

    Estudo: Objetos

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-// setInterval e Timeout
-
-function acao() {
-  document.write('Executando... <br/>');
-}
-
-var timer = setInterval(() => {
-  document.write('Executando!! <br/>');
-}, 1000);
 
 
-setTimeout(() => {
-  document.write('Executou o Timeout! <br/>');
-}, 3000);
+
+//Objetos
+
+let pessoa = {
+    nome: 'Aluno',
+    idade: 25,
+    altura: 1.76,
+    cargo: 'Desenvolvedor'
+};
+
+let carro = {
+    nome: 'Fusca',
+    cor: 'Branco',
+    potencia: '140cv'
+};
+
+
+let usuarios = [
+    {nome: 'Mateus', cargo: 'Programador Frontend', status: 'Ativo'},
+    {nome: 'Maria', cargo: 'Programadora Backend', status: 'Ativo'},
+    {nome: 'Felipe', cargo: 'Programador Mobile', status: 'Inativo'}
+];
+
+
+console.log(usuarios);
