fc483ed3b30a7c74afa71a38da0bf9052eb25901
Mateus Zancheta <mateus.zancheta@uniplaclages.edu.br>
2026-08-12 20:25:27 -0300

    Estudo: Includes, startsWith e endsWith

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 
-//Funcoes anonimas
+//Includes, startsWith, endsWith
 
-function somar(a, b) {
-    let total = a + b;
+let nomes = ['Mateus', 'Lucas', 'Jose'];
 
-    return console.log(total);
+console.log(nomes.includes('Maria'));
+
+if(nomes.includes('Mateus')) {
+    console.log('Esta na lista!');
+}
+else {
+    console.log('Não esta na lista!');
 }
 
-somar(10,30);
+//---------------------------------------
+let nome = 'Mateus';
 
-//Funcao anonima
-let subtrair = (valor1, valor2) => {
-    let total = valor1 - valor2;
+console.log(nome.startsWith('Mat'));
 
-    return console.log(total);
-}
-
-subtrair(50, 25);
-
-let numeros = [1, 3, 5, 10];
-
-numeros.map((item) => {
-    console.log(item);
-})
+console.log(nome.endsWith('eus'));
